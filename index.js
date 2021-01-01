99ae833903e9d766057d3a904e6c9e09a9372464
Raúl López <rldona@gmail.com>
2021-01-01 22:32:38 +0100

    added new feature

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const puppeteer = require('puppeteer');
 const admin = require("firebase-admin");
+const getProxies = require("get-free-https-proxy");
+
 const filmaffinityScrapper = require("./filmaffinity-scraper");
 
 const serviceAccount = require("./filmaffinity-api-firebase-adminsdk-hfsxr-99032fbdcb.json");
@@ -11,7 +13,7 @@ admin.initializeApp({
 
 const config = {
   db: admin.firestore(),
-  headless: true,
+  headless: false,
   view: {
     width: 1024,
     height: 2500
@@ -25,17 +27,22 @@ const config = {
 
 const reviewsRef = config.db.collection(`reviews-${config.language}`);
 
-async function scrappingFilmaffinty (id) {
+async function scrappingFilmaffinty (proxy, url) {
   const browser = await puppeteer.launch({
     headless: config.headless,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    browserContext: "default"
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      `--proxy-server=${proxy.host}:${proxy.port}`
+    ]
   });
 
-  const page = await browser.newPage();
-  await page.setViewport({ width: config.view.width, height: config.view.height });
+  console.log(proxy);
 
-  const url = `https://www.filmaffinity.com/${config.language}/film${id}.html`;
+  const context = await browser.createIncognitoBrowserContext();
+  const page = await context.newPage();
+
+  await page.setViewport({ width: config.view.width, height: config.view.height });
 
   ////
 
@@ -53,25 +60,60 @@ async function scrappingFilmaffinty (id) {
 
   ////
 
+  let review = null;
+
   let browserLoad = await page.goto(url, { waitUntil: 'load', timeout: 0 });
 
   console.log(browserLoad.status());
 
-  if (browserLoad.status() !== 404) {
-    const review = await filmaffinityScrapper.init(page, browser);
-    // await reviewsRef.doc(`${id}`).set({ id, ...review, url });
-    console.log(`==> ${id} == ${review.title} | OK <==`);
-  } else {
-    console.log(`==> ${id} | KO <==`);
+  if (browserLoad.status() === 200) {
+    review = await filmaffinityScrapper.init(page, browser);
   }
 
   await browser.close();
+
+  return {
+    data: review,
+    status: browserLoad.status()
+  };
 }
 
 (async () => {
 
-  for (let id = config.range.start; id < config.range.end; id++) {
-    await scrappingFilmaffinty(id);
+  // DigitalOcean (Doplets 5€)
+  // AWS Free
+
+  let url;
+  let review;
+  let id = config.range.start;
+
+  // get proxy list
+  const proxies = await getProxies();
+
+  while (id < config.range.end) {
+    url = `https://www.filmaffinity.com/${config.language}/film${id}.html`;
+
+    const randomProxy = proxies[Math.floor(Math.random() * (proxies.length - 0 + 1)) + 0];
+
+    // TODO: sustituir por el fetch/await de URL de la function Firebase y pasar id
+    review = await scrappingFilmaffinty(randomProxy, url);
+
+    if (review.status === 429) {
+      // Reintentar con el mismo id y con una IP difernete desde la function
+      // TODO: sustituir por el fetch/await de URL de la function Firebase y pasar id
+      review = await scrappingFilmaffinty(url);
+    }
+
+    if (review.status === 404) {
+      console.log(`==> ${id} | KO <==`);
+      id++;
+    }
+
+    if (review.status === 200) {
+      console.log(`==> ${id} == ${review.data.title} <==`);
+      await reviewsRef.doc(`${id}`).set({ id, ...review, url });
+      id++;
+    }
   }
 
   console.log('==> Carga completa <==');
